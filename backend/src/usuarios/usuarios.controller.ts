6dcf1d98573e92b6bd87ff73f9b7368cdda7cf13
carloospg <carlosperezgosalvez@gmail.com>
2026-05-05 18:28:54 +0200

    feat: añado el endpoint de eliminar usuarios

--- backend/src/usuarios/usuarios.controller.ts
+++ backend/src/usuarios/usuarios.controller.ts
@@ -1,18 +1,24 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { UsuariosService } from './usuarios.service';
-import { JwtAuthGuard } from '../guards/jwt-auth.guard';
-import { RolesGuard } from '../guards/roles.guard';
-import { Roles } from '../decorators/roles.decorator';
+import { Controller, Delete, Get, Param, UseGuards } from "@nestjs/common";
+import { UsuariosService } from "./usuarios.service";
+import { JwtAuthGuard } from "../guards/jwt-auth.guard";
+import { RolesGuard } from "../guards/roles.guard";
+import { Roles } from "../decorators/roles.decorator";
 
-@Controller('api/usuarios')
+@Controller("api/usuarios")
 export class UsuariosController {
-
   constructor(private readonly usuariosService: UsuariosService) {}
 
   @Get()
   @UseGuards(JwtAuthGuard, RolesGuard)
-  @Roles('admin')
+  @Roles("admin")
   listar() {
     return this.usuariosService.listarUsuarios();
+  }
+
+  @Delete(":id")
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles("admin")
+  eliminar(@Param("id") id: string) {
+    return this.usuariosService.eliminarUsuario(Number(id));
   }
 }
