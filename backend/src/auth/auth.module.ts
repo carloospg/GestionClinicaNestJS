bd60475f09c56b9f4b31820fa0d62d99e5aa01dd
carloospg <carlosperezgosalvez@gmail.com>
2026-05-05 16:54:49 +0200

    feat: añado el registro de usuario

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./jwt.strategy";
 
 @Module({
   imports: [
@@ -13,7 +14,7 @@ import { AuthService } from "./auth.service";
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   exports: [JwtModule],
 })
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { PrismaService } from "src/prisma/prisma.service";
 import { LoginDto } from "./dto/login.dto";
 import * as bcrypt from "bcryptjs";
+import { RegistroDto } from "./dto/registro.dto";
 
 @Injectable()
 export class AuthService {
@@ -38,6 +43,39 @@ export class AuthService {
     return {
       ok: true,
       token,
+      usuario: {
+        id: usuario.id,
+        nombre: usuario.nombre,
+        email: usuario.email,
+        rol: usuario.rol,
+      },
+    };
+  }
+
+  async registro(registroDto: RegistroDto) {
+    const { nombre, email, password, rol } = registroDto;
+
+    if (rol === "admin") {
+      throw new ConflictException("No se puede crear un usuario con rol admin");
+    }
+
+    const existe = await this.prisma.usuario.findUnique({
+      where: { email },
+    });
+
+    if (existe) {
+      throw new ConflictException("El usuario ya existe");
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    const usuario = await this.prisma.usuario.create({
+      data: { nombre, email, password: hash, rol },
+    });
+
+    return {
+      ok: true,
+      msg: "Usuario creado correctamente",
       usuario: {
         id: usuario.id,
         nombre: usuario.nombre,
