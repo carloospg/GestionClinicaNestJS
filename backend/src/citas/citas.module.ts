05553160e662b68fea671599a8c499ed25139f64
carloospg <carlosperezgosalvez@gmail.com>
2026-05-15 12:07:41 +0200

    feat: añado websockets en el citas service y controller

--- backend/src/citas/citas.module.ts
+++ backend/src/citas/citas.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { CitasController } from "./citas.controller";
 import { CitasService } from "./citas.service";
 import { PrismaModule } from "../prisma/prisma.module";
+import { EventosModule } from "src/eventos/eventos.module";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, EventosModule],
   controllers: [CitasController],
   providers: [CitasService],
 })
